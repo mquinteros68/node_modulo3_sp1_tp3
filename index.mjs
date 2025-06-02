378ac389ce6d4b857f51709ae0cc2986f6de7d5d
mquinteros68 <m_quinteros@yahoo.com>
2025-06-02 01:51:16 -0300

    trabajo practico n3

---
+++ index.mjs
@@ -0,0 +1,9 @@
+
+import {leerSuperheroes, agregarSuperheroes} from './utils.mjs'; 
+const archivoOriginal = './superheroes.txt'; 
+const archivoNuevos = './agregarSuperheroes.txt'; 
+// Agregar nuevos superhéroes 
+agregarSuperheroes (archivoOriginal, archivoNuevos); 
+// Leer y mostrar la lista actualizada de superhéroes ordenada const superheroes = leer Superheroes (archivoOriginal); 
+console.log('Superhéroes ordenados:'); 
+console.log(superheroes);
